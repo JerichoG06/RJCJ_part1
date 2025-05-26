fd720dad05bd7d4d0636381de9877bd0d9c674c0
JerichoG06 <106004642@student.swin.edu.au>
2025-05-26 21:54:34 +1000

    commit

--- Downloads/RJCJ_part1-main/docs/project2/login.php
+++ Downloads/RJCJ_part1-main/docs/project2/login.php
@@ -27,6 +27,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <html>
 <head>
     <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="styles/styles.css">
 </head>
 <body>
 <h2>Login</h2>
